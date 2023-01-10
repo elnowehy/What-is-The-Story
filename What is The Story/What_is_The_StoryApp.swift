d9cdde6921c9b2c8afc4b8e6fa689b945a4ecb61
elnowehy <122034550+elnowehy@users.noreply.github.com>
2023-01-10 11:10:31 -0500

    Initial Commit

---
+++ What is The Story/What_is_The_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_is_The_StoryApp.swift
+//  What is The Story
+//
+//  Created by Amr El-Nowehy on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct What_is_The_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
