d706e716085b39cb4a8d7c495bf6f7f6cfe151f0
elnowehy <122034550+elnowehy@users.noreply.github.com>
2023-12-22 15:49:09 -0500

    updates the database. We still need to capture the error messages from the function.

--- Networking/Firebase/Functions/processTokenClaims.ts
+++ Networking/Firebase/Functions/processTokenClaims.ts
@@ -23,61 +23,81 @@ const provider = new ethers.JsonRpcProvider(rpcUrl);
 const wallet = new ethers.Wallet(privateKey, provider);
 
 export const processTokenClaims = async (userId: string, amount: number): Promise<void> => {
-    try {
-        const userDoc = await db.collection('User').doc(userId).get();
-        if (!userDoc.exists) {
-            throw new Error('User not found');
-        }
-        
-        const userWalletAddress = userDoc.data()?.wallet ?? "";
-        if(!userWalletAddress) {
-            throw new Error("User wallet address is undefined")
-        }
-        const contract = new ethers.Contract(contractAddress, contractABI, wallet);
-        
-        // Calculate the amount to transfer (assuming amount is in the correct unit)
-        const transferAmount = ethers.parseUnits(amount.toString(), 'ether');
-        
-        // Estimate gas limit for the transfer
-        const estimateGasLimit = await contract.transfer.estimateGas(userWalletAddress, transferAmount);
-        const defaultGasPrice = ethers.parseUnits("30", "gwei");
-        const estimatedGasFeeWei = defaultGasPrice * estimateGasLimit;
-        const estimatedGasFee = ethers.formatEther(estimatedGasFeeWei);
-        
-        // Check if the user has enough gas balance
-        const userGasBalanceDoc = await db.collection('Tokens_Balance').doc(userId).get();
-        if (!userGasBalanceDoc.exists) {
-            throw new Error('User gas balance not found');
-        }
-        let userGasBalance = userGasBalanceDoc.data()?.userGasBalance;
-        
-        if(userGasBalance < estimatedGasFee) {
-            //throw new Error(`not enough gas balance: ${userGasBalance} Required: ${estimatedGasFee}`);
+    await db.runTransaction(async (transaction) => {
+        try {
+            const userRef = db.collection('User').doc(userId);
+            const userDoc = await transaction.get(userRef)
+            if (!userDoc.exists) {
+                throw new functions.https.HttpsError('not-found', 'User document doesn not exit');
+            }
             
-            throw new functions.https.HttpsError('failed-precondition', `Not enough gas balance: ${userGasBalance} MATIC. Required: ${estimatedGasFee} MATIC`);
+            const userWalletAddress = userDoc.data()?.wallet ?? "";
+            if(!userWalletAddress) {
+                throw new Error("User wallet address is undefined")
+            }
+            const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+            
+            // Calculate the amount to transfer (assuming amount is in the correct unit)
+            const transferAmount = ethers.parseUnits(amount.toString(), 'ether');
+            
+            // Estimate gas limit for the transfer
+            const estimateGasLimit = await contract.transfer.estimateGas(userWalletAddress, transferAmount);
+            const defaultGasPrice = ethers.parseUnits("30", "gwei");
+            const estimatedGasFeeWei = defaultGasPrice * estimateGasLimit;
+            const estimatedGasFee = ethers.formatEther(estimatedGasFeeWei);
+            
+            // Check if the user has enough gas balance
+            const userGasBalanceRef = db.collection('Tokens_Balance').doc(userId);
+            const userGasBalanceDoc = await transaction.get(userGasBalanceRef)
+            if (!userGasBalanceDoc.exists) {
+                throw new Error('User gas balance not found');
+            }
+            let userGasBalance = userGasBalanceDoc.data()?.userGasBalance;
+            let userUnclaimed = userGasBalanceDoc.data()?.uncalimed;
+            let userClaimed = userGasBalanceDoc.data()?.claimed;
+            
+            if(userGasBalance < estimatedGasFee) {
+                throw new functions.https.HttpsError('failed-precondition', `Not enough gas balance: ${userGasBalance} MATIC. Required: ${estimatedGasFee} MATIC`);
+            }
+            console.log(`userGasBalance: ${userGasBalance}, estimatedGasFee: ${estimatedGasFee}`)
+            
+            // Call transfer function of your smart contract
+            const tx = await contract.transfer(userWalletAddress, transferAmount);
+            const receipt = await tx.wait();
+            console.log("receipt: ", receipt);
+            console.log("gas used", Number(receipt.gasUsed));
+            
+            // Deduct actual gas fee
+            const actualGasFeeWei = Number(receipt.gasUsed) * Number(receipt.gasPrice);
+            const actualGasFee = Number(ethers.formatEther(actualGasFeeWei.toString()));
+            console.log("actual gas fee: ", actualGasFee);
+            
+            userGasBalance -= Number(actualGasFee);
+            console.log("userGasBalance: ", userGasBalance);
+            
+            // update token balance for treasury and users
+            const treasuryRef = db.collection("Tokens").doc("Treasury")
+            const treasuryDoc = await transaction.get(treasuryRef)
+            if (!treasuryDoc.exists) {
+                throw new Error('Treasury data not found');
+            }
+            let treasuryUnclaimed = treasuryDoc.data()?.uncailmed
+            
+            // await db.collection('Tokens_Balance').doc(userId).update({ userGasBalance });
+            transaction.update(userGasBalanceRef, {
+                userGasBalance: userGasBalance,
+                claimed: userClaimed + amount,
+                uncalimed: userUnclaimed - amount,
+            })
+            
+            transaction.update(treasuryRef, {
+                unClaimed: treasuryUnclaimed - amount
+            })
+            
+            console.log(`Tokens transferred to ${userWalletAddress}: ${amount}`);
+        } catch (error) {
+            console.error("Claim processing failed: ", error)
         }
-        console.log(`userGasBalance: ${userGasBalance}, estimatedGasFee: ${estimatedGasFee}`)
-        
-        // Call transfer function of your smart contract
-        const tx = await contract.transfer(userWalletAddress, transferAmount);
-        const receipt = await tx.wait();
-        console.log("receipt: ", receipt);
-        console.log("gas used", Number(receipt.gasUsed));
-        
-        // Deduct actual gas fee
-        const actualGasFeeWei = Number(receipt.gasUsed) * Number(receipt.gasPrice);
-        const actualGasFee = Number(ethers.formatEther(actualGasFeeWei.toString()));
-        console.log("actual gas fee: ", actualGasFee);
-        
-        userGasBalance -= Number(actualGasFee);
-        console.log("userGasBalance: ", userGasBalance);
-        await db.collection('Tokens_Balance').doc(userId).update({ userGasBalance });
-        
-        console.log(`Tokens transferred to ${userWalletAddress}: ${amount}`);
-    } catch (error) {
-        console.error("Claim processing failed: ", error)
-    }
-};
-
-// Add further logic as needed
-
+    })
+}
+                            
